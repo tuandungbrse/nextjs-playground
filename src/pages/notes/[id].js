1f2c4b6262549f0adc0173de1995ad3c3c917f52
tuandungbrse <tuandungbrse@gmail.com>
2021-08-03 22:47:31 +0700

    fix route css

--- src/pages/notes/[id].js
+++ src/pages/notes/[id].js
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-
+import Link from 'next/link'
 export default function NoteDetail() {
   const router = useRouter() // get parameters
 
@@ -7,7 +7,15 @@ export default function NoteDetail() {
 
   const { id } = router.query // same name with the file
 
-  return <div>Note Details {id}</div>
+  return (
+    <div>
+      <h1>Note: {id} </h1>
+
+      <Link href="/notes">
+        <a>Notes</a>
+      </Link>
+    </div>
+  )
 }
 
 // dynamic routing
--- src/pages/notes/index.js
+++ src/pages/notes/index.js
@@ -1,7 +1,23 @@
+import Link from 'next/link'
+
 export default function Notes() {
+  const notes = new Array(15)
+    .fill(1)
+    .map((e, i) => ({ id: i, title: `Note: ${i}` }))
+
   return (
     <div>
-      <h1>Hello, jsut a Component with route /notes</h1>
+      <h1>Notes</h1>
+
+      {notes.map((note) => (
+        <div key={note.id}>
+          <Link key={note.id} href="/notes/[id]" as={`/notes/${note.id}`}>
+            <a>
+              <strong>{note.title}</strong>
+            </a>
+          </Link>
+        </div>
+      ))}
     </div>
   )
 }
